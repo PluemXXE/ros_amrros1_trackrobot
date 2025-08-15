1cab2c3c63eb44645d61ba4848c127208bd0fa7a
osp <phahkinpluemza101@gmail.com>
2025-08-15 15:53:28 +0700

    initial

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/osp/xiaor_robotics/src
-# Build directory: /home/osp/xiaor_robotics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("transbot_description")
-subdirs("xiaorkinematics")
--- build/xiaorkinematics/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/osp/xiaor_robotics/src/xiaorkinematics
-# Build directory: /home/osp/xiaor_robotics/build/xiaorkinematics
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
